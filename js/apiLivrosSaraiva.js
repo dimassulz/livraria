9f52fe1aa76fc6daf7b60544edae04d35b70da59
Dimas <dimas.filho@sistemafibra.org.br>
2018-09-11 21:30:05 -0300

    apiSaraiva listando os livros

--- js/apiLivrosSaraiva.js
+++ js/apiLivrosSaraiva.js
@@ -1,11 +1,23 @@
-$(function(){
+$(function () {
     $.ajax({
-        url:"https://api.saraiva.com.br//sc/produto/collection/10121686%7C9875730%7C9358803%7C9873991%7C9955275%7C10121687%7C8881153%7C9389693%7C4238667%7C9919331%7C10088927%7C1416781%7C10116877%7C10125836/0/0/1/",
+        url: "https://api.saraiva.com.br//sc/produto/collection/10121686%7C9875730%7C9358803%7C9873991%7C9955275%7C10121687%7C8881153%7C9389693%7C4238667%7C9919331%7C10088927%7C1416781%7C10116877%7C10125836/0/0/1/",
         dataType: 'json',
-        success: function(objLivros){
-            console.log(objLivros)
+        success: function (arrLivros) {
+            console.log(arrLivros)
+            $.each(arrLivros, function (i, livro) {
+                $('#livros').append(`
+            <div class="col-lg-4">
+              <img class="img-thumbnail" src="${livro.image}">
+              <h3>${livro.name}</h3>
+              <p>${livro.authors[0].name}</p>
+              <p>De R$ ${livro.price.original}</p>
+              <h2>Por R$ ${livro.price.value}</h2>
+              <p><a class="btn btn-secondary" href="#" role="button">Ver detalhes &raquo;</a></p>
+            </div>
+            `);
+            })
         },
-        error: function(){
+        error: function () {
             alert('Ocorreu um erro ao retornar os livros da Saraiva!');
         }
     })
